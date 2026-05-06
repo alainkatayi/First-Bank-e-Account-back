590e6fd125846cb890a5124537280e512dd5f62f
alainkatayi <alainkatayi01@gmail.com>
2026-05-06 15:11:53 +0100

    feat(Decision): Implémentation de la vue TakeDecisionView, qui permet de faire la prise de decision

--- request/urls.py
+++ request/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RequestCreatedView, RequestListView
+from .views import RequestCreatedView, RequestListView, TakeDecisionView
 
 urlpatterns = [
     path('store', RequestCreatedView.as_view(), name='soumission'),
     path('index', RequestListView.as_view(), name='get-all-request'),
+    path('<int:request_id>/decision', TakeDecisionView.as_view(), name='take-decision'),
 ]
--- request/views.py
+++ request/views.py
@@ -1,7 +1,7 @@
 from rest_framework import status
 from rest_framework.response import Response
 from .models import SupportingDocument
-from .serializers import RequestSerializer
+from .serializers import RequestSerializer, DecisionSerializer
 from rest_framework.views import APIView
 from rest_framework.permissions import  AllowAny, IsAuthenticated
 from rest_framework import status
@@ -49,3 +49,32 @@ class RequestListView(ListAPIView):
     #on verifie l'authentifiation et le role
     permission_classes = [IsAuthenticated, IsAdminOrAgent]
     pagination_class = Pagination
+
+#class pour la prise de decision sur une request(demande d'ouverture)
+#deux cas possible: refus ou acceptation
+class TakeDecisionView(APIView):
+    permission_classes = [IsAuthenticated, IsAdminOrAgent]
+    def post(self, request, request_id):
+        try:
+            #la recupere la requete à traiter
+            request_to_process = Request.objects.get(id=request_id)
+        except Request.DoesNotExist:
+            #si la requette n'est pas trouvé
+            return Response({"error": "Demande introuvable"}, status=status.HTTP_404_NOT_FOUND)
+        #on verifie si une decision a deja ete rendu sur cette demande
+        if hasattr(request_to_process, 'decision'):
+            return Response({"error": "Une décision a déjà été rendue pour ce dossier"}, status=status.HTTP_400_BAD_REQUEST)
+        serializer = DecisionSerializer(data=request.data)
+        if serializer.is_valid():
+            try:
+                with transaction.atomic():
+                    decision = serializer.save(
+                        request=request_to_process,
+                        agent=request.user
+                    )
+                    request_to_process.status = decision.decision_type
+                    request_to_process.save()
+                return Response({"message": "Décision enregistrée avec succès"}, status=status.HTTP_201_CREATED)
+            except Exception as e:
+                return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
