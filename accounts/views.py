f4ac27c93153ccac83a282e9458670fdea42b9bd
alainkatayi <alainkatayi01@gmail.com>
2026-05-08 15:45:52 +0100

    feat(accounts): Implémentation de la vue UserListView

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.views import APIView as ApiView
 from accounts.serializers import RegisterSerializer, CustomTokenObtainPairSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
+from .models import User
+from .serializers import UserSerializer
+from abstracts_models.permission import IsAdminUserRole
+from rest_framework.permissions import IsAuthenticated
+from abstracts_models.pagination import Pagination
 # Create your views here.
 class RegisterView(ApiView):
     permission_classes = [AllowAny]
@@ -21,3 +27,9 @@ class CustomTokenObtainPairView(TokenObtainPairView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception = True)
         return Response(serializer.validated_data, status=200)
+
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated, IsAdminUserRole]
+    pagination_class = Pagination
