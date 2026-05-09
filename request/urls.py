09c0c42e522479f8ba80c13b4c99c76e43ad398b
alainkatayi <alainkatayi01@gmail.com>
2026-05-09 23:56:19 +0100

    feat(request): Implémentation de la vue RequestDetailView

--- request/urls.py
+++ request/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RequestCreatedView, RequestListView, TakeDecisionView, DecisionListView
+from .views import RequestCreatedView, RequestListView, TakeDecisionView, DecisionListView, RequestDetailView
 
 urlpatterns = [
     path('store', RequestCreatedView.as_view(), name='soumission'),
     path('index', RequestListView.as_view(), name='get-all-request'),
+    path('<int:id>', RequestDetailView.as_view(), name='get-one-request'),
     path('<int:request_id>/decision', TakeDecisionView.as_view(), name='take-decision'),
     path('decision', DecisionListView.as_view(), name='take-decision'),
 ]
